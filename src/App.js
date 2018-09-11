9c971a6bb0fe5454aa476f3838856df5a2d0d43d
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2018-09-11 15:01:34 +0200

    Hook up TODO reducer to React

--- src/App.js
+++ src/App.js
@@ -2,32 +2,68 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
+import * as actionTypes from './actions/actionTypes';
 import './App.css';
-import Counter from './components/Counter';
+
+let nextTodoId = 0;
 
 class App extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      text: ''
+    }
+  }
+  onChange = (event) => {
+    const { name, value } = event.target;
+    // console.log(event.target.name);
+    // console.log(event.target.value);
+    const newState = Object.assign({}, this.state);
+    newState[name] = value;
+    this.setState(newState);
+  }
+
+  addTodo = () => {
+    const { text } = this.state;
+    const { dispatch } = this.props;
+    dispatch({
+      type: actionTypes.ADD_TODO,
+      todo: {
+        id: nextTodoId++,
+        text
+      }
+    });
+    this.setState({ text: '' });
+  }
+
   render() {
-    const { counter, dispatch } = this.props;
+    const { todos } = this.props;
+    const { text } = this.state;
     return (
       <div className="App">
-        <Counter
-          counter={counter.counter}
-          onIncrement={() => dispatch({ type: 'INCREMENT' })}
-          onDecrement={() => dispatch({ type: 'DECREMENT' })}
-        />
+        <input name="text" onChange={this.onChange} value={text} />
+        <button onClick={this.addTodo}>Add todo</button>
+        <ul>
+          {
+            todos.map(todo => {
+              return <li key={todo.id}>{todo.text}</li>
+            })
+          }
+        </ul>
       </div>
     );
   }
 }
 
 App.propTypes = {
-  counter: PropTypes.object.isRequired,
+  todos: PropTypes.array.isRequired,
   dispatch: PropTypes.func.isRequired
 }
 
 function mapStateToProps(state, ownProps) {
   return {
-    counter: state
+    todos: state.todos
   }
 }
 
